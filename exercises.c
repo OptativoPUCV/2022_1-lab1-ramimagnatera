d5e4bbc50a1876782158b6d06e1626ca6fbe75c1
RamiroMagnater1 <>
2022-03-15 00:06:50 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -85,7 +85,7 @@ typedef struct {
 
 Vector* crearVector(int n){
   
-  Vector* p = (Vector*) malloc(n*sizeof(Vector));
+  Vector* p = (Vector*) calloc(n*sizeof(Vector));
   if(p == 0) exit(EXIT_FAILURE);
   
   return p;
